cc3a661e0d55ccfe042ff2a03119ce61679602eb
Surbhikumari-2109 <surbhipriya2109@gmail.com>
2026-06-26 05:24:49 +0530

    Update API base URL

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,3 +1,5 @@
 export const PORT = process.env.PORT || 5000;
 export const MONGO_URI = process.env.MONGO_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
+
+VITE_API_BASE_URL='https://jansetu-eta0.onrender.com';
